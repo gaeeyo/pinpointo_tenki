ebd45c62f6f3d5c7cba000693baada19919f2a86
gaeeyo <iknnofm0126@gmail.com>
2010-09-10 15:51:17 +0900

    ヘルプをhtmlにした

--- src/nikeno/Tenki/HelpActivity.java
+++ src/nikeno/Tenki/HelpActivity.java
@@ -3,11 +3,15 @@ package nikeno.Tenki;
 import nikeno.Tenki.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class HelpActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.help);
-	}
+		
+		WebView webView = (WebView)findViewById(R.id.webView); 
+		webView.loadUrl("file:///android_asset/help.html");
+	}  
 }
