098d574a1bbfcd7b02964fa26b67e2484b621efb
gaeeyo <iknnofm0126@gmail.com>
2012-01-13 23:44:17 +0900

    ウィジェットの更新間隔を3時間から1時間に変更。ウィジェット更新時にサービスがすぐに終了するように

--- src/nikeno/Tenki/TenkiWidgetProvider.java
+++ src/nikeno/Tenki/TenkiWidgetProvider.java
@@ -1,28 +1,28 @@
 package nikeno.Tenki;
 
-import nikeno.Tenki.activity.TenkiWidgetConfigure;
-import nikeno.Tenki.service.WidgetUpdateService;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
+
+import nikeno.Tenki.activity.TenkiWidgetConfigure;
+import nikeno.Tenki.service.WidgetUpdateService;
 
 public class TenkiWidgetProvider extends AppWidgetProvider {
-	private static final String TAG = TenkiWidgetProvider.class.getSimpleName();
-	
+
+	static final String TAG = TenkiWidgetProvider.class.getSimpleName();
+
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
-		
-		Log.d(TAG, "onUpdate");
-		context.startService(new Intent(context, WidgetUpdateService.class));
+
+		WidgetUpdateService.setAlarm(context);
 	}
-	
+
 	@Override
 	public void onDeleted(Context context, int[] appWidgetIds) {
 		super.onDeleted(context, appWidgetIds);
 		TenkiWidgetConfigure.deleteWidgetConfig(context, appWidgetIds);
 	}
+
 }
